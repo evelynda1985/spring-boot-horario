c5d7ecdcc33f98a5bb688ad5e156558c4af41e8b
evelynda1985 <eagredo@mum.edu>
2020-07-26 17:55:36 -0400

    Initial commit

---
+++ src/main/java/com/evecode/springboot/horariointerceptor/app/MvcConfig.java
@@ -0,0 +1,25 @@
+package com.evecode.springboot.horariointerceptor.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    @Qualifier("horario")
+    private HandlerInterceptor horario;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+        //excludePathPatterns("/cerrado") siempre hacer para el camino alternativo del inteceptor - return false
+        registry.addInterceptor(horario).excludePathPatterns("/cerrado");
+
+    }
+}
